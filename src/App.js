2412175f4379e79c2e6e72d1bc122de599184d64
mini2809 <minijain928@gmail.com>
2022-03-28 16:54:47 +0530

    Todo App completed

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
 import './App.css';
-import  Input  from "./inputToDo.js";
-import Display from "./displayTodo.js";
 import { useState } from "react";
-
 
 function App() {
 
-  let [todos, addTodo ] = useState(
-    [
-      {
-          "id":1,
-          "description":"Take Bath",
-      },
-      {
-          "id":2,
-          "description":"Frontend Interviews Prep",
-      },
-      {
-          "id":3,
-          "description":"Watch Netflix",
-      },
-      {
-          "id":4,
-          "description":"Play Badminton",
-      }
-    ]
-  )
-  
+    let [todos, setTodo ] = useState("");
+    let[todoId , setId] = useState(0);
+    let [todoList, setList] = useState([]);
+
   return (
     <div className="App">
       <div className="heading">My TODO-App</div>
-        <Input />
-        <Display todos={todos}/>
+      <div>
+            <div className="input-container">
+                <input type="text" placeholder="Whats on your List? Add it !" className="input" id="input" value={todos} onChange={(e)=>setTodo(e.target.value)}></input>
+            </div>
+            <button className="submit" onClick={()=> {
+              setTodo("");
+              setId(todoId+1);
+              setList([...todoList,{"id":todoId,"description":todos}]);
+            }}>Add Task</button>
+        </div>
+        <div className="todos">
+            <div className="subheading">My Todos</div>
+            <ul>
+                {todoList.map((todo)=>(
+                    <li key={todo.id}>
+                        <div className='todoItems'>
+                            <div id={todo.id} className="description">{todo.description}</div>
+                            <div id={"cross"+ todo.id} className="cross" onClick={()=>
+                              HandleDeleteTodo(todo.id,todoList,setList)
+                            }> x </div>
+                        </div>
+                        
+                    </li>
+                 ))}
+                </ul>
+        </div>
     </div>
   );
+}
+function HandleDeleteTodo(deleteId ,todoList,setList){
+  let newList=[];
+  todoList.map((td)=>{
+      if(td.id != deleteId)
+      newList.push(td);
+  })
+  setList(newList);
 }
 
 export default App;
